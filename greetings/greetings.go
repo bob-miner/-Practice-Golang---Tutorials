8c14e4e6360787570453e19951823c0f4cbb18e0
namdp2 <namdaophuong@tech.admicro.vn>
2024-10-15 00:52:47 +0700

    Feat: Handle return random greeting and greeting for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,36 @@ package greetings
 import (
 	"fmt"
 	"errors"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hello, %v. Welcome!", name);
+	message := fmt.Sprintf(randomFormat(), name);
 	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, error := Hello(name)
+		if error != nil {
+			return nil, error
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v.Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
--- hello-world/hello.go
+++ hello-world/hello.go
@@ -9,8 +9,9 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-
-	message, error := greetings.Hello("")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	message, error := greetings.Hellos(names)
+	
 	if error != nil {
 		log.Fatal(error)
 	}
